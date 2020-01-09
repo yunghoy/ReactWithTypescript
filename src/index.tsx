853ce33c1072f61e6ca8f306743156d765337198
Youngho Lee <yunghoy@uci.edu>
2020-01-09 01:40:28 -0800

    Learn and Be Curious

---
+++ src/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+interface MyProps {
+    name: string;
+}
+
+interface MyState {
+    data: string;
+}
+
+class ShoppingList extends React.Component<MyProps, MyState> {
+
+    constructor(props: MyProps) {
+        super(props);
+        this.state = {
+            data: "started"
+        }
+    }
+
+    async componentDidMount(): Promise<void> {
+        try {
+            const response: Response = await fetch('https://api.github.com/users/mralexgray/repos');
+            if (!response.ok) throw Error(response.statusText);
+
+            const json: any = await response.json();
+            const data: string = JSON.stringify(json);
+
+            this.setState({data})
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    public render(): any {
+        return (
+            <div className="shopping-list">
+                <h1>Shopping List for {this.props.name}</h1>
+                <ul>
+                    <li>Apple</li>
+                    <li>{this.state.data}</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <ShoppingList name="Peter"/>,
+    document.getElementById('root')
+);
